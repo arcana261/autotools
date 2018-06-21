97c5390a9d2027e3d115eceb69555422b18aaad3
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2018-06-21 18:35:31 +0430

    added a whine library :D

---
+++ src/whine/whine.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include "whine.h"
+
+void do_whine(void) {
+    printf("whine whine whine!\n");
+}
---
+++ src/whine/whine.h
@@ -0,0 +1,6 @@
+#ifndef __WHINE_H__
+#define __WHINE_H__
+
+void do_whine(void);
+
+#endif
